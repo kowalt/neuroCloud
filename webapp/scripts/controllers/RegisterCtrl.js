e4c9199b9b21c2daf8914ccf48e2d0a100634437
Tomasz Kowal <kowaltt@hotmail.com>
2015-08-06 09:20:54 -0400

    Workbench header stub. TODO: Fix content, fix logo

--- webapp/scripts/controllers/RegisterCtrl.js
+++ webapp/scripts/controllers/RegisterCtrl.js
@@ -1,7 +1,28 @@
 'use strict';
 
 angular.module('nncloud')
-  .controller('RegisterCtrl',['$scope', function ($scope) {
-	
-
+  .controller('RegisterCtrl',['$scope', '$http', '$location', function ($scope,$http,$location) {
+	$scope.register = function()
+	{
+		var request = 
+		{
+			method: "POST",
+			url: API['AUTH_REGISTER'],
+			headers:
+			{
+				"Content-Type": "application/json"
+			},
+			data: { "email": $scope.user.email, "login": $scope.user.login, "password": $scope.user.password, "message_to_admin": $scope.user.message_to_admin }
+		}
+		$http(request).success(function(data, status, headers, config)
+		{
+			Materialize.toast("Data has been submitted successfully. We will look at your request.", 9000);
+			$location.path('/login');
+		})
+		.error(function(data, status, headers, config)
+		{
+			Materialize.toast(data.error, 9000);
+		});
+		
+	}
   }]);
