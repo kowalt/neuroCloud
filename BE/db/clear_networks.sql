44be634785ef2961bd94fbbd207a5f098c23f1d4
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2015-09-19 01:51:52 +0200

    Script for clearing networks

---
+++ BE/db/clear_networks.sql
@@ -0,0 +1,9 @@
+use nncloud;
+
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE activation_functions;
+TRUNCATE TABLE synapses;
+TRUNCATE TABLE neurons;
+TRUNCATE TABLE layers;  
+TRUNCATE TABLE networks;
+SET FOREIGN_KEY_CHECKS = 1;
