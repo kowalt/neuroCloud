44493e328c62eb8eae6b42cb50af02be543a2c59
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-02-27 18:16:18 +0100

    Finished ValueCalculator. Need to be tested.

--- GPUserver/nncGPUServer/src/main/java/normal/ValueCalculator.java
+++ GPUserver/nncGPUServer/src/main/java/normal/ValueCalculator.java
@@ -1,7 +1,10 @@
 package normal;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import model.original.ActivationFunction;
+import normal.expcalc.FunctionInterpreter;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,14 +19,21 @@ import model.original.ActivationFunction;
 
 public class ValueCalculator
 {
+    private FunctionInterpreter f_interpreter;
+    
+    public ValueCalculator()
+    {
+        f_interpreter = new FunctionInterpreter();
+    }
+    
     public double calculateValue(List<ActivationFunction> activation_functions, double input)
     {
         double output = Double.NaN;
         for(ActivationFunction af: activation_functions)
         {
-            if(checkIfDomainRuleApply(af.getDomain_rule()))
+            if(checkIfDomainRuleApply(af.getDomain_rule(), input))
             {    
-                output = calculateFunctionValue(af.getFunction(),input);
+                output = calculateFunctionValue(af.getFunction(), input);
                 break;
             }
         }
@@ -34,21 +44,62 @@ public class ValueCalculator
      * Check if any of rules apply
      * @author Tomasz Kowal
     */
-    private boolean checkIfDomainRuleApply(String domain_rules)
+    private boolean checkIfDomainRuleApply(String domain_rules, double x_value)
     {
+        Set<String> comparators = new HashSet<String>();
+        
+        comparators.add(">");
+        comparators.add(">=");
+        comparators.add("<");
+        comparators.add("<=");
+        comparators.add("=");
+
         String[] domain_rules_arr = domain_rules.split(",");
         
+        //check conjunction
         for(String domain_rule: domain_rules_arr)
         {
-            
+            for(String c: comparators)
+            {    
+                String[] rightAndLeft = domain_rule.split(c);
+                if(rightAndLeft!=null)
+                {   
+                    String right = rightAndLeft[1];
+                    String left = rightAndLeft[0];
+                    if(isRuleTrue(left, right, c, x_value))
+                        return true;
+                }
+            }
         }
-        
+     
         return false;
     }
     
+    private boolean isRuleTrue(String left, String right, String comparator, double x)
+    {   
+        double leftValue = calculateFunctionValue(left, x);
+        double rightValue = calculateFunctionValue(right, x);
+        
+        switch(comparator)
+        {    
+            case ">":
+                return leftValue > rightValue;
+            case ">=":
+                return leftValue >= rightValue;
+            case "<=":
+                return leftValue <= rightValue;
+            case "<":
+                return leftValue <= rightValue;
+            case "=":
+                return leftValue == rightValue;
+            default:
+                return false;
+        }
+    }
+
     private double calculateFunctionValue(String funcion_raw, double argument)
     {
-        return Double.NaN;
+        return f_interpreter.calculateValueInfix(funcion_raw, new double[]{argument});
     }
 }
 
