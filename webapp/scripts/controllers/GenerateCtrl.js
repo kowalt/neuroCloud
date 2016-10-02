c8c6679d7b6753749da85b6435f01234b919d0a6
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-10-03 01:29:29 +0200

    Small bugfixes

--- webapp/scripts/controllers/GenerateCtrl.js
+++ webapp/scripts/controllers/GenerateCtrl.js
@@ -6,7 +6,7 @@ angular.module('nncloud')
 		{
 			if(!validate())
 				return;
-			
+
 			var request = 
 			{
 				method: "POST",
@@ -17,7 +17,7 @@ angular.module('nncloud')
 				},
 				data: {"name": $scope.network.name ,"1stlayer": $scope.network.first, "2ndlayer": $scope.network.first, "3rdlayer": $scope.network.third, "4thlayer": $scope.network.fourth, "activationFunction": $scope.network.activationFunction }
 			};
-			
+
 			$http(request).success(function(data, status, headers, config)
 			{
 				$alert({title: 'Network generated', content: 'Network generation successfull', placement: 'top', type: 'info', show: true});				
@@ -27,7 +27,7 @@ angular.module('nncloud')
 				$alert({title: 'Unable to generate', content: data, placement: 'top', type: 'danger', show: true});
 			});
 		}
-		
+
 		var validate = function()
 		{
 			var error_message = "";
@@ -39,10 +39,10 @@ angular.module('nncloud')
 				error_message = "Neuron number in layer must not be empty!";
 			
 			if(error_message != )
-			
+			{
 				$alert({title: 'Cannot register: ', content: error_message, placement: 'top', type: 'danger', show: true});
 				return false;
 			}
 			return true;
 		}
-  });
+  }]);
