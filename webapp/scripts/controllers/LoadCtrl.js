91a17763e168b2d8ef59196bed0a16142fcd1a71
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-10-09 01:25:42 +0200

    Bugfix

--- webapp/scripts/controllers/LoadCtrl.js
+++ webapp/scripts/controllers/LoadCtrl.js
@@ -2,14 +2,15 @@
 
 app
   .controller('LoadCtrl', ['$scope', '$cookies', '$location', '$alert', 'networksService', function ($scope, $cookies, $location, $alert, networksService) {
-	networksService.getNetworksList().success(function(data)
+	try
 	{
-		$scope.networks = data;
-	}).error(function(err)
+		$scope.networks = networksService.getNetworksList();
+	}
+	catch(err)
 	{
 		$alert({title: 'Unable to load list of networks', content: err, placement: 'top', type: 'danger', show: true});
-	});
-	
+	}
+
 	$scope.selectNetwork = function(id)
 	{
 		$cookies.put('activeNetworkID', id);
