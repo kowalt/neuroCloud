69dc39319e34fac90ad3d862d87a304086a61acb
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-10-10 01:24:52 +0200

    Started making DTO and transformations.

---
+++ BE/nnCloudRESTService/src/main/java/com/mycompany/nncloudrestservice/logic/NetworkTransform.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.nncloudrestservice.logic;
+
+import com.mycompany.nncloudrestservice.pojo.Network;
+
+/**
+ * Class responsible for transformations Network->NetworkDTO, NetworkDTO->Network
+ * @author Tomasz
+ */
+public class NetworkTransform {
+   public void transformIn(Network network)
+   {
+       
+   }
+   
+   public Network transformOut()
+   {
+       return null;
+   }
+}
