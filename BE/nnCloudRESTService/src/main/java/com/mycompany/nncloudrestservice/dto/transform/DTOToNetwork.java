9e769d844e3c7c6ce94e96aa80022e53787260d4
kowalt@ee.pw.edu.pl <tomasz.kowal@accenture.com>
2016-10-24 17:23:15 +0200

    Started synapse transformation

--- BE/nnCloudRESTService/src/main/java/com/mycompany/nncloudrestservice/dto/transform/DTOToNetwork.java
+++ BE/nnCloudRESTService/src/main/java/com/mycompany/nncloudrestservice/dto/transform/DTOToNetwork.java
@@ -14,10 +14,12 @@ import com.mycompany.nncloudrestservice.daos.NetworkDAO;
 import com.mycompany.nncloudrestservice.dto.LayerDTO;
 import com.mycompany.nncloudrestservice.dto.NetworkDTO;
 import com.mycompany.nncloudrestservice.dto.NeuronDTO;
+import com.mycompany.nncloudrestservice.dto.SynapseDTO;
 import com.mycompany.nncloudrestservice.pojo.ActivationFunction;
 import com.mycompany.nncloudrestservice.pojo.Layer;
 import com.mycompany.nncloudrestservice.pojo.Network;
 import com.mycompany.nncloudrestservice.pojo.Neuron;
+import com.mycompany.nncloudrestservice.pojo.Synapse;
 import com.mycompany.nncloudrestservice.utils.CurrentUserContainer;
 
 /**
@@ -51,6 +53,8 @@ public class DTOToNetwork {
        n.setName(ndto.getName());
        n.setUser(CurrentUserContainer.getInstance());
        n.setLayers(transformLayers(ndto));
+
+       transformSynapses();
        
        return n;
     }
@@ -92,6 +96,7 @@ public class DTOToNetwork {
     		neuron.setId(ndt.getId());
     		neuron.setLayer(layer);
     		neuron.setActivation_functions(transformActivation_functions(ndt.getActivation()));
+
         	neuList.add(neuron);
     	}
     	return neuList;
@@ -112,5 +117,25 @@ public class DTOToNetwork {
     		afl.add(af);
     	}
     	return afl;
+    }
+    
+    private void transformSynapses()
+    {
+    	List<Synapse> synInput = transformSingleSynapsesList(ndto.getSynapsesInput());
+    	List<Synapse> syn12 = transformSingleSynapsesList(ndto.getSynapsesBetween1and2Layer());
+    	List<Synapse> syn23 = transformSingleSynapsesList(ndto.getSynapsesBetween2and3Layer());
+    	List<Synapse> syn34 = transformSingleSynapsesList(ndto.getSynapsesBetween3and4Layer());
+    	List<Synapse> synOutput = transformSingleSynapsesList(ndto.getSynapsesOutput());
+    }
+    
+    private List<Synapse> transformSingleSynapsesList(List<SynapseDTO> synDTOList)
+    {
+    	List<Synapse> synList = new ArrayList<>();
+    	
+    	for(SynapseDTO sdt: synDTOList)
+    	{
+    		Synapse s = new Synapse();
+    		s.setId(sdt.getId()); //TODO finish
+    	}
     }
 }
