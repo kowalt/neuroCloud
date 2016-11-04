2507a6ffcc3c6059bcac5c1354e285bd2bc21d22
kowalt@ee.pw.edu.pl <tomasz.kowal@accenture.com>
2016-11-04 17:19:57 +0100

    Started implementation of training

---
+++ webapp/scripts/controllers/TrainingCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+app
+  .controller('TrainingCtrl', ['$scope', '$cookies', '$alert', '$location', 'networksService','commonDataService', function ($scope, $cookies, $alert, $location,networksService,commonDataService) {
+	
+}]);
