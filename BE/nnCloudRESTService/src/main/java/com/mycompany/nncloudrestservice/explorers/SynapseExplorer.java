d1d9310a69ac26f412dead16a268f0f08d63a15f
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-11-28 02:57:59 +0100

    Advancing with training implementation

---
+++ BE/nnCloudRESTService/src/main/java/com/mycompany/nncloudrestservice/explorers/SynapseExplorer.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.nncloudrestservice.explorers;
+
+import com.mycompany.nncloudrestservice.pojo.Layer;
+import com.mycompany.nncloudrestservice.pojo.Network;
+import com.mycompany.nncloudrestservice.pojo.Neuron;
+import com.mycompany.nncloudrestservice.pojo.Synapse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Tomasz
+ */
+public class SynapseExplorer {
+    private Network network;
+    private Map<Integer, Synapse> registry;
+    
+    public SynapseExplorer(Network n)
+    {
+        this.network = n;
+        registry = new HashMap<Integer, Synapse>();
+    }
+    
+    private boolean isInRegistry(Integer synId)
+    {
+        return registry.get(synId) != null;
+    }
+    
+    /**
+     * Returns all unique synapses
+    */
+    public List<Synapse> getAll()
+    {        
+
+        for(Layer layer: network.getLayers())
+            for(Neuron neuron: layer.getNeurons())
+            {
+                for(Synapse synIn: neuron.getSynapses_in())
+                {
+                    if(isInRegistry(synIn.getId()))
+                        registry.put(synIn.getId(), synIn);
+                }
+                
+                for(Synapse synOut: neuron.getSynapses_out())
+                {
+                    if(isInRegistry(synOut.getId()))
+                        registry.put(synOut.getId(), synOut);
+                }
+            }
+        List<Synapse> synList = new ArrayList<>();
+        synList.addAll(registry.values());
+        return synList;
+    }
+    
+    public void saveAll(Integer networkdId)
+    {
+    
+    }
+}
--- BE/nnCloudRESTService/src/main/java/com/mycompany/nncloudrestservice/training/ebp/EBP.java
+++ BE/nnCloudRESTService/src/main/java/com/mycompany/nncloudrestservice/training/ebp/EBP.java
@@ -1,25 +1,59 @@
 package com.mycompany.nncloudrestservice.training.ebp;
 
+import com.mycompany.nncloudrestservice.localcalculations.singlethreaded.SingleThreadRunManager;
 import com.mycompany.nncloudrestservice.pojo.Network;
+import java.util.List;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.ArrayUtils;
 
-public class EBP {
-	private Network network;
-	private Double learning_factor;
-	private Integer iterations;
-	private Double[] learning_set;
-	private Double[] training_set;
-	
-	public EBP(Network network, Double learning_factor, Integer iterations, Double[] learning_set, Double[] training_set)
-	{
-		this.network = network;
-		this.learning_factor = learning_factor;
-		this.iterations = iterations;
-		this.learning_set = learning_set;
-		this.training_set = training_set;
-	}
-	
-	public void train(Network n)
-	{
-		
-	}
+public class EBP implements Runnable {
+    private Network network;
+    private Double learning_factor;
+    private Integer iterations;
+    private List<Double[]> learning_set;
+    private List<Double[]> training_set;
+
+    public EBP(Network network, Double learning_factor, Integer iterations, List<Double[]> learning_set, List<Double[]> training_set)
+    {
+        this.network = network;
+        this.learning_factor = learning_factor;
+        this.iterations = iterations;
+        this.learning_set = learning_set;
+        this.training_set = training_set;
+    }
+
+    private double calculateGoalFunction(double[] output_vector, double[] expected_vector)
+    {
+        double r = 0.0;
+
+        for(int i=0; i<output_vector.length; i++)
+            r += Math.pow(output_vector[i] - expected_vector[i], 2);
+
+        return r/2.0;
+    }
+    
+    private void updateWeights(double goal_function)
+    {
+        
+    }
+    
+    private void train(Network n)
+    {
+        SingleThreadRunManager runManager = new SingleThreadRunManager(network);
+        for(int i=0; i<iterations; i++)
+        {
+            for(int j=0; j<learning_set.size(); j++)
+            {    
+                double[] output_vector = runManager.run(ArrayUtils.toPrimitive(learning_set.get(j)));
+                double[] expected_vector = ArrayUtils.toPrimitive(training_set.get(j));
+                double goal_function = calculateGoalFunction(output_vector, expected_vector);
+                updateWeights();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        train(network);
+    }
 }
