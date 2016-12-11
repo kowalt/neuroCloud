488d00116fb15dd5023ebce3a88e65e6dffd3fa7
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-12-12 00:28:14 +0100

    File upload in angularJS manner

--- webapp/scripts/controllers/TrainingCtrl.js
+++ webapp/scripts/controllers/TrainingCtrl.js
@@ -4,39 +4,12 @@ app
   .controller('TrainingCtrl', ['$scope', '$alert', 'networksService','commonDataService', function ($scope, $alert,networksService,commonDataService) {
 	$scope.trainingProps = {};
 
-	function digestCSV(rawContent)
-	{
-		var retArr = [];
-		var arrayOfLines = rawContent.match(/[^\r\n]+/g);
-		
-		for(var i=0; i<arrayOfLines; i++)
-			retArr[i] = arrayOfLines[i].split(',');
-			
-		return retArr;
-	}
-
 	function obtainNetworkId(networkXML)
 	{
 		parser = new DOMParser();
 		xmlDoc = parser.parseFromString(networkXML, "application/xml");
 		var network = xmlDoc.getElementsByTagName("network");
 		return network.getAttribute('id');
-	}
-
-	$scope.addLearningData = function() {
-		var f = document.getElementById('learningSet').files[0], r = new FileReader();
-		r.onloadend = function(e) {
-			$scope.trainingProps.learningSet = digestCSV(e.target.result);
-			r.readAsBinaryString(f);
-		}
-	}
-
-	$scope.addTrainingData = function() {
-		var f = document.getElementById('trainingSet').files[0], r = new FileReader();
-		r.onloadend = function(e) {
-			$scope.trainingProps.trainingSet = digestCSV(e.target.result);
-			r.readAsBinaryString(f);
-		}
 	}
 
 	$scope.train = function() {
---
+++ webapp/scripts/directives/trainingDataFileReader.js
@@ -0,0 +1,34 @@
+app.directive('trainingDataFileReader', function() {
+  return {
+	restrict: 'A',
+    link: function(scope, element, attrs) {
+      $(element).on('change', function(changeEvent) {
+        var files = changeEvent.target.files;
+        if (files.length) {
+          var r = new FileReader();
+          r.onload = function(e) {
+              var contents = e.target.result;
+              scope.$apply(function () {
+				function digestCSV(rawContent)
+				{
+					var retArr = [];
+					var arrayOfLines = rawContent.match(/[^\r\n]+/g);
+					
+					for(var i=0; i<arrayOfLines; i++)
+						retArr[i] = arrayOfLines[i].split(',');
+						
+					return retArr;
+				}
+				if(scope.$eval(attrs.fileType) === 'learning')
+					scope.trainingProps.learningSet = digestCSV(contents);
+				if(scope.$eval(attrs.fileType) === 'training')
+					scope.trainingProps.trainingSet = digestCSV(contents);
+              });
+          };
+
+          r.readAsText(files[0]);
+        }
+      });
+    }
+  };
+});
