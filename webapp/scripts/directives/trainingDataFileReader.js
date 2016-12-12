b25032a912ee45f1781e8dae26115a97c084bb53
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2016-12-12 01:08:02 +0100

    Bugfix

--- webapp/scripts/directives/trainingDataFileReader.js
+++ webapp/scripts/directives/trainingDataFileReader.js
@@ -3,7 +3,7 @@ app.directive('trainingDataFileReader', function() {
 	restrict: 'A',
 	scope: {},
     link: function(scope, element, attrs) {
-      $(element).on('change', function(changeEvent) {
+      element.on('change', function(changeEvent) {
         var files = changeEvent.target.files;
         if (files.length) {
           var r = new FileReader();
