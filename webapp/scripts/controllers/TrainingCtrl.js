7b21c520b7c97ab87f8a8665384761e4552a237c
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2017-06-23 03:58:18 +0200

    Quick bugfix

--- webapp/scripts/controllers/TrainingCtrl.js
+++ webapp/scripts/controllers/TrainingCtrl.js
@@ -32,7 +32,7 @@ app
 			
 			$scope.percentage = iterationsDone/iterationsMax * 100;
 			
-			progressMessage = $scope.percentage.toString()+"%("+iterationsDone+"/"+iterationsMax+")";
+			$scope.progressMessage = $scope.percentage.toString()+"%("+iterationsDone+"/"+iterationsMax+")";
 		}).error(function(err){
 			$alert({title: 'Unable to check if network is currently trained', content: err, placement: 'top', type: 'danger', show: true });
 		});
