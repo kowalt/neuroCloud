0a649e49df0ec1c9b648d147e3debd6675dece49
Tomasz Kowal <kowalt@ee.pw.edu.pl>
2017-07-05 02:49:19 +0200

    Service now implemented in project convention

--- webapp/scripts/services/localizedMessageService.js
+++ webapp/scripts/services/localizedMessageService.js
@@ -1,52 +1,57 @@
 app
 .factory('localizedMessageService', ['$cookies', function($cookies) {
-	return
+	var exposedAPI =
 	{
-		getDropdown: function()
-		{
-			return [
-				{text: 'English', click: '$cookies.put("language","en")'},
-				{text: 'Polish', click: '$cookies.put("language","pl")'}
-			  ];
-		},
-		
-		getLocalizedMessage: function(messageCode)
-		{
-			var langCode = $cookies.get('language');
+		getDropdown: getDropdown,
+		getLocalizedMessage: getLocalizedMessage
+	}
+	
+	return exposedAPI;
+	
+	function getDropdown()
+	{
+		return [
+			{text: 'English', click: '$cookies.put("language","en")'},
+			{text: 'Polish', click: '$cookies.put("language","pl")'}
+		  ];
+	}
+	
+	function getLocalizedMessage(messageCode)
+	{
+		var langCode = $cookies.get('language');
 
-			switch(langCode)
-			{
-				case "en":
-					return getEnValue(messageCode);
-					break;
-				case "pl":
-					return getPlValue(messageCode);
-					break;
-			}
-			
-			return "Localization service error";
-		},
-		
-		getEnValue: function(messageCode)
+		switch(langCode)
 		{
-			switch(messageCode)
-			{
-				case "settings.header":
-					return "SETTINGS";
-			}
-			
-			return "Localization service error getting English value";
-		},
-
-		getPlValue: function(messageCode)
-		{
-			switch(messageCode)
-			{
-				case "settings.header":
-					return "USTAWIENIA"
-			}
-
-			return "Localization service error getting Polish value";
+			case "en":
+				return getEnValue(messageCode);
+				break;
+			case "pl":
+				return getPlValue(messageCode);
+				break;
 		}
+		
+		return "Localization service error";
+	}
+	
+	function getEnValue(messageCode)
+	{
+		switch(messageCode)
+		{
+			case "settings.header":
+				return "SETTINGS";
+		}
+		
+		return "Localization service error getting English value";
+	}
+
+	function getPlValue(messageCode)
+	{
+		switch(messageCode)
+		{
+			case "settings.header":
+				return "USTAWIENIA"
+		}
+
+		return "Localization service error getting Polish value";
 	}
 }]);
